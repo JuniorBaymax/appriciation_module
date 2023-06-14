ebdb843fa78407bb63ae86ce3fa4532d7f5eb5a0
aman.jha1 <aman.jha@teamcomputers.com>
2023-06-14 20:05:37 +0530

    Added api services and api client

---
+++ src/models/auth_type.ts
@@ -0,0 +1,31 @@
+export type LoginCredentials = {
+  username: string;
+  password: string;
+};
+
+export type changePasswordType = {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+};
+
+export type verifyOtpType = {
+  email: string;
+  otp: string;
+};
+
+export type userPassword = {
+  newPassword: string;
+  email: string;
+};
+
+export type AuthResponse = {
+  Data: { Full_Name: string };
+};
+
+export type changePassword = {
+  data: any;
+  message: string;
+  statusCode: number;
+  success: boolean;
+};
---
+++ src/models/user.ts
@@ -0,0 +1,13 @@
+export interface UserData {
+  Full_Name: string;
+  First_Name: string;
+  TMC: string;
+  Phone_No: string;
+  Mobile_Phone_No: string;
+  Personal_E_Mail: string;
+  Location_Code: string;
+  Gender: number;
+  Designation: string;
+  Department: string;
+  Company_E_Mail: string;
+}
